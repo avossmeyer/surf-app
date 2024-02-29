dd197f0d17910aaf9888d234ac1c643cc970840e
avossmeyer <alexvossmeyer@gmail.com>
2024-02-29 15:11:42 -0800

    Update app2.py

--- code/app2.py
+++ code/app2.py
@@ -9,7 +9,8 @@ import plotly.express as px
 
 @st.cache_data
 def pull_data():
-
+    
+    from queries import app_query
 
     HEADERS = ({'User-Agent':
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
@@ -23,11 +24,7 @@ def pull_data():
                                     database="postgres")
 
     cursor = connection.cursor()
-    from queries import app_query
-    # x = input.input_box()
-    # print(x)
     cursor.execute(app_query.format('LAX', 'LAX'))
-
     airports_data = cursor.fetchall()
     cursor.close()
 
@@ -35,39 +32,22 @@ def pull_data():
     df['Rating'] = df['Rating'].astype(float)
     df['airport_lat'] = df['airport_lat'].astype(float)
     df['airport_lon'] = df['airport_lon'].astype(float)
+    
     return df
 
 def run(df):
-    # import pdb; pdb.set_trace()
-
-    # return df[['Airport', 'City', 'Rating', 'Ordering', 'Spot Dist', 'Spots', '% 5+',
-    #        'avg_of_best_session_for_each_break', 'Best', 'max_10d_rating',
-    #        'num_good_surf_spots', 'avg_dist', 'array_agg', 'Out Price',
-    #        'Return Price']].fillna('')
-
-    # option = st.selectbox(
-    #     "Home Airport",
-    #     # ("LAX", ),
-    #     ("SAN", "LAX", "SFO"),
-    #     index=None,
-    #     placeholder="Select Your Home Airport...",
-    # )
     df.index += 1 
 
     st.title("Best Surf Worldwide (Where to Fly)")
     
-    # st.write(df[['City', 'Airport', 'Rating', 'Best', 'Outbound', 'Return', 'Breaks', 'Dist to Breaks']])
     x = df[['City', 'Airport', 'Rating', 'Best', 'Breaks', 'Dist to Breaks']]
-    x.columns = ['City', 'Airport', 'Average Rating', 'Best Rating', 'Number of Breaks', 'Dist to Breaks']
+    x.columns = ['City', 'Airport', 'Average Rating', 'Best Rating', 'Number of Breaks', 'Distance to Breaks']
+    
     st.write(x)
 
-    # map_df = df.nlargest(15, 'Rating')
-    # map_df = map_df[['airport_lat', 'airport_lon', 'Airport']]
-    # map_df.columns = ['lat', 'lon', 'Airport']
     n_spots = 15
     n_spots = st.slider('How many airports do you want to show?', 0, df.shape[0], n_spots)
 
-    # st.map(df.nlargest(n_spots, 'Rating'), latitude='airport_lat', longitude='airport_lon')
     n_spots_df = df.nlargest(n_spots, 'Rating')
     n_spots_df['Avg Dist Airport to Break'] = n_spots_df['Dist to Breaks']
 
@@ -80,16 +60,9 @@ def run(df):
             'airport_lon': False,
         },
     )
-    # df = px.data.carshare()
-    # fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
-    #                         color_continuous_scale=px.colors.cyclical.IceFire, size_max=10, zoom=10)
 
     fig.update_layout(mapbox_style="open-street-map")
-    # fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     st.plotly_chart(fig)
-
-    # st.write("I'm ", age, 'years old')
-
 
 if __name__ == '__main__':
     df = pull_data()
